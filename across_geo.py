d8c0279bc7ad470d9cfd9f1210d6f56df9f5d930
Jacob Lee Miller <jacobmiller1@cs.arizona.edu>
2022-05-18 13:21:39 -0700

    Adding sgd images.

--- across_geo.py
+++ across_geo.py
@@ -17,8 +17,8 @@ hyper_geo = lambda u,v: acosh( cosh(u[1])*cosh(v[0]-u[0])*cosh(v[1]) - sinh(u[1]
 euclid_geo = lambda u,v: np.linalg.norm(u-v)
 
 def generate_spherical_data(n=100):
-    x1 = np.random.uniform(0,math.pi, (n,1) )
-    x2 = np.random.uniform(0,2*math.pi, (n,1) )
+    x1 = np.random.uniform(0,2*math.pi, (n,1) )
+    x2 = np.random.uniform(0,math.pi, (n,1) )
     return np.concatenate( (x1,x2), axis=1 )
 
 def generate_unit_circle(n=100):
@@ -61,7 +61,7 @@ def compare(sizes = (20,100,10), iter=5):
     i,j,k = sizes
     num_data = [num for num in range(i,j,k)]
 
-    for geom in ['euclidean', 'spherical', 'hyperbolic']:
+    for geom in ['spherical']:
         data = np.zeros( (iter*len(num_data), 3) )
         metric = euclid_geo if geom == 'euclidean' else sphere_geo if geom == 'spherical' else hyper_geo
         print(geom)
@@ -72,7 +72,7 @@ def compare(sizes = (20,100,10), iter=5):
             else:
                 X = generate_unit_circle(n=num_data[size])
 
-            d = dist_matrix(X,metric=metric)
+            d = dist_matrix(X,metric=sphere_geo)
             print(num_data[size])
 
             for a in range(iter):
@@ -83,7 +83,7 @@ def compare(sizes = (20,100,10), iter=5):
                 time_e = end-start
 
                 start = time.perf_counter()
-                X_s = SMDS(d).solve(1000)
+                X_s = SMDS(d).solve(num_iter=1000)
                 end = time.perf_counter()
                 time_s = end-start
 
@@ -91,61 +91,65 @@ def compare(sizes = (20,100,10), iter=5):
                 X_h = HMDS(d).solve(1000)
                 end = time.perf_counter()
                 time_h = end-start
+                print("E: {}, S: {}, H: {}".format(distortion(X_e,d,euclid_geo), distortion(X_s,d,sphere_geo), distortion(X_h, d, hyper_geo)))
 
                 data[size*iter+a] = np.array([distortion(X_e,d,euclid_geo),
                                               distortion(X_s,d,sphere_geo),
                                               distortion(X_h, d, hyper_geo)])
 
+
+
         header_txt = "Starting from {}, size increases by {} every {} rows. Columns are euclid, sphere, hyperbolic".format(i,k,iter)
         np.savetxt("data/{}_results.csv".format(geom),data,delimiter=',',header=header_txt)
 
-#import autograd.numpy as np
-nlog, nabs, nconj = np.log, np.absolute, np.conj
-
-def poin_dist1(q,p):
-    top = ( nabs( 1- np.conj(q)*p ) + nabs( p-q ) )
-    bottom = nlog( nabs( 1- np.conj(q)*p ) - nabs( p-q ) )
-    return top-bottom
-
-def poin_dist2(q,p):
-    return nlog( ( nabs( 1- np.conj(q)*p ) + nabs( p-q ) ) / ( nabs( 1- np.conj(q)*p ) - nabs( p-q ) ) )
-
-def poin_dist3(q,p):
-    diff = abs(p-q)
-    first = abs( 1- q.conjugate()*p )
-    return math.log( (first+diff)/ (first-diff))
-
-import_mod = '''import numpy as np; import math; nlog, nabs, nconj = np.log, np.absolute, np.conj; import random;
-from __main__ import poin_dist1;
-from __main__ import poin_dist2;
-from __main__ import poin_dist3'''
-
-try1 = '''
-poin_dist1(random.random(),random.random())
-'''
-
-try2 = '''
-poin_dist2(random.random(),random.random())
-'''
-
-try3 = '''
-poin_dist3(random.random(),random.random())
-'''
-
-u = 0+0j
-v = 0.09+0.1j
-print(poin_dist1(u,v))
-print(poin_dist2(u,v))
-print(poin_dist3(u,v))
-import timeit
-print("Method 1 takes: {}".format( timeit.timeit(setup = import_mod,
-                     stmt = try1,
-                     number = 10000)/10000 ))
-
-print("Method 2 takes: {}".format( timeit.timeit(setup = import_mod,
-                     stmt = try2,
-                     number = 10000)/10000 ))
-
-print("Method 3 takes: {}".format( timeit.timeit(setup = import_mod,
-                     stmt = try3,
-                     number = 10000)/10000 ))
+compare(sizes=(100,1001,100),iter=3)
+# #import autograd.numpy as np
+# nlog, nabs, nconj = np.log, np.absolute, np.conj
+#
+# def poin_dist1(q,p):
+#     top = ( nabs( 1- np.conj(q)*p ) + nabs( p-q ) )
+#     bottom = nlog( nabs( 1- np.conj(q)*p ) - nabs( p-q ) )
+#     return top-bottom
+#
+# def poin_dist2(q,p):
+#     return nlog( ( nabs( 1- np.conj(q)*p ) + nabs( p-q ) ) / ( nabs( 1- np.conj(q)*p ) - nabs( p-q ) ) )
+#
+# def poin_dist3(q,p):
+#     diff = abs(p-q)
+#     first = abs( 1- q.conjugate()*p )
+#     return math.log( (first+diff)/ (first-diff))
+#
+# import_mod = '''import numpy as np; import math; nlog, nabs, nconj = np.log, np.absolute, np.conj; import random;
+# from __main__ import poin_dist1;
+# from __main__ import poin_dist2;
+# from __main__ import poin_dist3'''
+#
+# try1 = '''
+# poin_dist1(random.random(),random.random())
+# '''
+#
+# try2 = '''
+# poin_dist2(random.random(),random.random())
+# '''
+#
+# try3 = '''
+# poin_dist3(random.random(),random.random())
+# '''
+#
+# u = 0+0j
+# v = 0.09+0.1j
+# print(poin_dist1(u,v))
+# print(poin_dist2(u,v))
+# print(poin_dist3(u,v))
+# import timeit
+# print("Method 1 takes: {}".format( timeit.timeit(setup = import_mod,
+#                      stmt = try1,
+#                      number = 10000)/10000 ))
+#
+# print("Method 2 takes: {}".format( timeit.timeit(setup = import_mod,
+#                      stmt = try2,
+#                      number = 10000)/10000 ))
+#
+# print("Method 3 takes: {}".format( timeit.timeit(setup = import_mod,
+#                      stmt = try3,
+#                      number = 10000)/10000 ))
--- vizdata.py
+++ vizdata.py
@@ -6,7 +6,7 @@ def read_and_parse(filename, iter=2, trait=1):
     x = data[:,trait]
     return x.reshape( (len(data) // iter, iter ) ).mean(axis=1)
 
-f_name = 'data/hyperbolic_results.csv'
+f_name = 'data/spherical_results.csv'
 iter = 5
 trait = 1
 
@@ -16,9 +16,13 @@ sphere = read_and_parse(f_name,iter=iter,trait=1)
 hyper = read_and_parse(f_name,iter=iter,trait=2)
 
 x = [i*100+100 for i in range(len(euclid))]
-plt.plot(x,euclid,label="Euclidean MDS")
-plt.plot(x,sphere,label="SMDS")
-plt.plot(x,hyper,label="HMDS")
-plt.legend()
-plt.ylim(0,0.1)
+plt.plot(x,euclid,label="Euclidean")
+plt.plot(x,sphere,label="Spherical")
+plt.plot(x,hyper,label="Hyperbolic")
+plt.suptitle("Sampled from 2d spherical space")
+#plt.yscale('log')
+plt.ylabel("Distortion")
+plt.xlabel("Number of data points")
+plt.legend(title='Embedding space')
+# plt.ylim(0,0.1)
 plt.show()
